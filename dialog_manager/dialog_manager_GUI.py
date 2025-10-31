81b0e1901ae04db2c0069ed1db31dc7949cf8228
DiveshLala <divesh.lala@gmail.com>
2025-10-31 11:14:51 +0900

    Conversations can be logged
    Updated dialogue manager GUI to include a conversation logging function

--- dialog_manager/dialog_manager_GUI.py
+++ dialog_manager/dialog_manager_GUI.py
@@ -1,4 +1,5 @@
-from tkinter import Tk, Frame, BOTH, Label, Button, Text, END
+from tkinter import Tk, Frame, BOTH, Label, Button, Text, Checkbutton, BooleanVar, END
+import datetime
 
 class GUI(Frame):
 	def __init__(self, controller):
@@ -7,12 +8,16 @@ class GUI(Frame):
 		self.dialog_controller = controller
 		self.dialog_controller.setGUI(self)
 		self.GUI = self.load_GUI(self.root)
+		self.log_conversation = False
+		self.log_file = None
 		self.root.wm_protocol("WM_DELETE_WINDOW", self.quit_GUI)
 		self.root.mainloop()
 		self.root.quit()
 	
 
 	def quit_GUI(self):
+		if self.log_conversation and self.log_file != None:
+			self.log_file.close()
 		self.GUI = None
 		self.root.destroy()
 		self.dialog_controller.quit_application()
@@ -30,6 +35,7 @@ class GUI(Frame):
 
 		self.make_ASR_input()
 		self.robot_response_display()
+		self.log_check()
 
 		self.isLoaded = True
 	
@@ -48,8 +54,26 @@ class GUI(Frame):
 		self.robot_speech = Label(self.root, text="", wraplength=400, justify="left", font=("Arial", 12)) 
 		self.robot_speech.place(x = 30, y = 80)
 	
+	def log_check(self):
+		self.log_var = BooleanVar()
+		self.log_var.set(False)
+		self.log_check = Checkbutton(self.root, text="Log conversation (new file will be created)", font=("Arial", 10), variable=self.log_var, command=self.set_log) 
+		self.log_check.place(x = 30, y = 120)
+	
+	def set_log(self):
+		self.log_conversation = self.log_var.get()
+		if self.log_conversation:
+			dt_now = datetime.datetime.now()
+			start_time_str = dt_now.strftime('%Y%m%d%H%M%S')
+			self.log_file = open(start_time_str + ".txt", "a", encoding="utf-8")
+		elif self.log_file != None:
+			self.log_file.close()
+			self.log_file = None
+	
 	def show_robot_speech(self, text):
 		self.robot_speech.config(text=text)
+		if self.log_conversation:
+			self.log_file.write("ROBOT: " + text + "\n")
 	
 	def update_turn_state(self, turn_state):
 		num_char = len(self.input_utterance.get('1.0', 'end-1c'))
@@ -60,3 +84,5 @@ class GUI(Frame):
 		if len(utterance) > 0:
 			self.input_utterance.delete('1.0', END)
 			self.dialog_controller.send_utterance_message(utterance)
+			if self.log_conversation:
+				self.log_file.write("HUMAN: " + utterance + "\n")
